ae405ce5c50046b8e39f71d9e553bbcb1f15552a
Alex Smith <amsmith1109@gmail.com>
2022-05-26 12:40:07 -0700

    Fix directories

--- microPy.py
+++
@@ -1,9 +0,0 @@
-from serial import Serial
-
-class uPy:
-    def __init__(self, obj):
-        serial = obj
-        
-    def write(self, message):
-        message = str(message)
-        self.serial.write(message + '\n\r')
