e045a62616732a3a5b134891e03cd5b8890cac2f
Alex Smith <amsmith1109@gmail.com>
2022-06-27 14:44:11 -0700

    Add return line to valve control

--- src/pre_con.py
+++ src/pre_con.py
@@ -7,7 +7,7 @@ class pre_con:
                  vc_port = '/dev/ttyACM0',
                  mfc_port = '/dev/ttyUSB2',
                  ads_trap_port = '/dev/ttyUSB0',
-                 h20_trap_port = '/dev/ttyUSB1'):
+                 h2o_trap_port = '/dev/ttyUSB1'):
         
         # It is very important that the USB configuration matches the usb ports
         # Improvements should be made to automatically determine where each
@@ -16,23 +16,25 @@ class pre_con:
         self.vc_port = vc_port
         self.mfc_port = mfc_port
         self.ads_trap_port = ads_trap_port
-        self.h20_trap_port = h20_trap_port
+        self.h2o_trap_port = h2o_trap_port
         
         self.vc = uPy(vc_port)
         self.mfc = uPy(mfc_port)
         #self.ads_trap = omegatc(ads_trap_port)
-        #self.h20_trap = omegatc(h20_trap_port)
+        #self.h2o_trap = omegatc(h2o_trap_port)
         
     def valve(self, V, position):
         if isinstance(V, int):
-            self.vc.write(f'v[{V}]({position})')
+            self.vc.write(f'v[{V}]({position})\r')
         elif isinstance(V, list) or isinstance(V, range):
             for n, i in enumerate(V):
                 if isinstance(position, int):
-                    self.vc.write(f'v[{i}]({position})')
+                    self.vc.write(f'v[{i}]({position})\r')
                 else:
-                    self.vc.write(f'v[{i}]({position[n]})')
+                    self.vc.write(f'v[{i}]({position[n]})\r')
         
 if __name__ == '__main__':
     pc = pre_con()
-    pc.valve(range(0,8),1)
+    #pc.valve(range(0,8),1)
+    #ads = pc.h2o_trap
+    v = pc.vc
