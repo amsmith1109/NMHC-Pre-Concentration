898040b15c005cfed1ca6a9eaa37a670708b57e1
Alex Smith <amsmith1109@gmail.com>
2022-08-17 17:03:03 -0700

    Update MFC Controller

--- src/mass_flow_controller/boot.py
+++ src/mass_flow_controller/boot.py
@@ -1,11 +1,22 @@
 from machine import Pin, SPI, I2C
 from ADS1X15 import ADS1115
 from MAX5134 import MAX5134
-#from mfc import mfc as _mfc
+from mfc import massFlowController
 i2c = I2C(scl=Pin(5), sda=Pin(4))
 addr = i2c.scan() # Assuming only ADS1115 is connected, otherwise specify the address
 adc = ADS1115(i2c, addr[0])
-adc.gain = 1
+adc.gain = 0
 spi = SPI(1, baudrate=400000)
 dac = MAX5134(Pin(15, Pin.OUT), spi)
-#mfc = _mfc(DAC=dac, ADC=adc)
+
+sampleMFC = massFlowController(DAC=dac,
+                             ADC=adc,
+                             maxFlow=1000,
+                             units='SCCM',
+                             port=0)
+
+carrierMFC = massFlowController(DAC=dac,
+                                ADC=adc,
+                                maxFlow=500,
+                                units='SCCM',
+                                port=1)
--- src/mass_flow_controller/mfc.py
+++ src/mass_flow_controller/mfc.py
@@ -1,66 +1,103 @@
-class _mfc:
+import time
+
+class massFlowController:
     def __init__(self,
-                 DAC,
-                 ADC,
-                 maxFlow = 50,
+                 DAC=None,
+                 ADC=None,
+                 maxFlow = None,
+                 port = None,
                  units = 'ccpm',
-                 offset = 0,
-                 scale = 1):
-    
+                 input_range = None,
+                 output_range = None,
+                 timeout = 5):
+        
+        if DAC==None:
+            print('No DAC provided.')
+        if ADC==None:
+            print('No ADC provided.')
+        if port==None:
+            print('Please specify a port.')
+        
         self.dac = DAC
         self.adc = ADC
         self.maxFlow = maxFlow
         self.units = units
-        self.cal = linear_calibration(offset, scale)
-        
+        self.port = port
+        self.timeout = timeout #Note that these are in seconds
+        if maxFlow!=None:
+            if (input_range==None) & (output_range==None):
+                self.cal = linear_calibration([0, maxFlow], [0.2, 5])
+            else:
+                self.cal = linear_calibration(input_range, output_range)                
+        self.setpoint = 0
     
-    def flow(self, flow):
-        1
+    def flowrate(self, flow, waiting=None):
+        if flow > self.maxFlow:
+            flow = self.maxFlow
+            print('Output capped at {} {}.'.format(flow, self.units))
+        voltage = self.cal.convert(flow)
+        self.dac.write(self.port, voltage)
+        self.setpoint = flow
+        timeout = 0
+        if waiting:
+            count = 0
+            time_stop = time.time() + self.timeout
+            while (time.time() < time_stop):
+                reading = self.check()
+                if abs(reading - flow)/flow < 0.01:
+                    count += 1
+                else:
+                    count = 0
+                # The MFC is considered stable if it is <1% of the range
+                # after being polled 5 times in a row. This is to account
+                # for over/under-shoot.
+                if count > 5:
+                    timeout = time.time() - t_stop + self.timeout
+                    break
+                timeout = True
+                time.sleep(.1)
+            if timeout==True:
+                print('Setpoint not reached before the timeout of {} s.'.format(self.timeout))
+            else:
+                print('Setpoint reached in {} s.'.format(timeout))
         return
+    
+    def check(self):
+        voltage = self.adc.single(self.port)
+        flow = self.cal.invert(voltage)
+        return flow
     
 class linear_calibration:
     def __init__(self,
-                 offset = 0,
-                 scale = 1):
-        self.offset = offset
-        self.scale = scale
+                 input_range,
+                 output_range):
+        self.calibrate(input_range, output_range)
     
     # return y from y = mx + b (x is provided)
-    def convert(self, val):
-        return val*self.scale + self.offset
+    def convert(self, x):
+        y = x*self.scale + self.offset
+        return y
     
     # Return x from y = mx + b (y is provided)
-    def invert(self, val):
-        return (val - self.offset)/self.scale
+    def invert(self, y):
+        x = (y - self.offset)/self.scale
+        return x
     
     # Perform a regression on a set of inputs to obtain
     # the offset and scale parameters.
     # Using this instead of importing a library to save space.
-    def calibrate(self, x, y):
-        count = len(x)
-        if count != len(y):
+    def calibrate(self, flow_rate, voltage):
+        count = len(flow_rate)
+        if count != len(voltage):
             print('Inputs must be the same size!')
             return
-        sum_y = sum(y)
-        sum_x = sum(x)
-        sum_x2 = sum([i**2 for i in x])
-        sum_xy = sum([i*j for i,j in zip(x,y)])        
+        sum_y = sum(voltage)
+        sum_x = sum(flow_rate)
+        sum_x2 = sum([i**2 for i in flow_rate])
+        sum_xy = sum([i*j for i,j in zip(flow_rate, voltage)])        
         _delta = count*sum_x2 - sum_x**2
         scale = (count*sum_xy - sum_x*sum_y)/_delta
         offset = (sum_y*sum_x2 - sum_x*sum_xy)/_delta
         
         self.scale = scale
         self.offset = offset
-
-if __name__ == '__main__':
-    import random
-    x = []
-    y = []
-    for i in range(0,10):
-        x.append(i)
-        ii = 1+(random.random()-.5)*.2
-        y.append(2*i*ii+5)
-    a = linear_calibration()
-    a.calibrate(x,y)
-    print(a.scale)
-    print(a.offset)
