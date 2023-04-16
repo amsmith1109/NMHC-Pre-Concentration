13b8913e32a9f19c31e03025dafb57b062f29d70
Alex Smith <amsmith1109@gmail.com>
2023-04-15 18:25:59 -0700

    Update Remote.py

--- src/remote.py
+++ src/remote.py
@@ -10,14 +10,28 @@ def signal_handler(sig, frame):
 class remote():
     def __init__(self,
              # Declared pin = RPi GPIO, Pin comments are "RPi Pin #, HP5890 Pin #"
-             inPins =  {'ready NO':25,  # Pin 22, 9
-                        'start A':24},  # Pin 18, 8}
-             outPins = {'ready C':22,   # Pin 15, 5
-                        'start B':23,   # Pin 16, 7
+             inPins =  {'ready':25,  # Pin 22, 9
+                        'start':24},  # Pin 18, 8}
+             outPins = {'ready':22,   # Pin 15, 5
+                        'set start':23,   # Pin 16, 7
                         'start':17,     # Pin 11, 1
-                        'ready':18,     # Pin 12, 12
+                        'set ready':18,     # Pin 12, 12
                         'config':27}):  # Pin 13, 3
-        self.inPins = inPins
-        self.outPins = outPins
+
         GPIO.setmode(GPIO.BCM)
         
+        self.inKeys =  [x for x in inPins.keys()]
+        self.inPins =  [x for x in inPins.values()]
+        self.outKeys = [x for x in outPins.keys()]
+        self.outPins = [x for x in outPins.values()]
+        for i in self.inPins:
+            GPIO.setup(i, GPIO.IN)
+        for i in self.outPins:
+            GPIO.setup(i, GPIO.OUT, initial=0)
+        
+    def check_ready(self):
+        pin = self.inPins['ready']
+        return GPIO.input(pin)
+    
+if __name__ == '__main__':
+    rm = remote()
